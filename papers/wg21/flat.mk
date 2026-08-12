983999b54621b513537ffa5df45208b5450f9164
Steve Downey <sdowney2@bloomberg.net>
2026-08-12 18:10:29 -0400

    Squashed 'papers/wg21/' changes from eccdf8c4..4269f434
    4269f434 Improve paper layout make rules and tests
    7a81154f Split Makefile fragments by project layout
    
    git-subtree-dir: papers/wg21
    git-subtree-split: 4269f434ed688170d404be3324235768e82d5e77

--- papers/wg21/flat.mk
+++ papers/wg21/flat.mk
@@ -19,18 +19,22 @@
 #   make p2806r4.latex # builds generated/p2806r4.latex from p2806r4.md
 #   make p2806r4.pdf   # builds generated/p2806r4.pdf from p2806r4.md
 #
-#   make               # builds all the papers in all of the formats
+#   make               # builds all the papers in HTML format (default)
 #   make html          # builds all the papers in HTML format
 #   make latex         # builds all the papers in LaTeX format
 #   make pdf           # builds all the papers in PDF format
 #
-#   make clean         # deletes the OUTDIR (`generated` by default)
+#   make clean         # deletes generated files
 #
 # The following variables can be set before including this file:
 #
 #   - OUTDIR := <path/to/directory>
 #
 #     Build the papers to the specified directory instead of `generated`
+#
+#   - DEFAULT_FORMAT := <html|pdf|latex>
+#
+#     Default format built by `make` with no explicit target.
 #
 #   - DEFAULTS := <path/to/defaults.yaml>
 #
@@ -44,39 +48,43 @@
 
 OUTDIR ?= generated
 
+DEFAULT_FORMAT ?= html
+ifneq ($(filter $(DEFAULT_FORMAT),html pdf latex),$(DEFAULT_FORMAT))
+$(error DEFAULT_FORMAT must be one of html, pdf, latex)
+endif
+
 DEFAULTS ?= $(wildcard defaults.yaml)
 REQUIREMENTS ?= $(wildcard requirements.txt)
 
-include $(dir $(lastword $(MAKEFILE_LIST)))wg21.mk
+src := $(filter-out CHANGELOG.md LICENSE.md README.md, $(wildcard *.md))
+html_targets := $(src:.md=.html)
+pdf_targets := $(src:.md=.pdf)
+latex_targets := $(src:.md=.latex)
 
-override SRC := $(filter-out CHANGELOG.md LICENSE.md README.md, $(wildcard *.md))
+.PHONY: all html pdf latex
+all: $(DEFAULT_FORMAT)
+html: $(html_targets)
+pdf: $(pdf_targets)
+latex: $(latex_targets)
 
-override HTML := $(SRC:.md=.html)
-override LATEX := $(SRC:.md=.latex)
-override PDF := $(SRC:.md=.pdf)
+include $(dir $(lastword $(MAKEFILE_LIST)))base.mk
 
-.PHONY: all
-all: $(HTML) $(LATEX) $(PDF)
-
-.PHONY: html
-html: $(HTML)
-
-.PHONY: latex
-latex: $(LATEX)
-
-.PHONY: pdf
-pdf: $(PDF)
+targets := $(html_targets) $(pdf_targets) $(latex_targets)
 
 .PHONY: clean
 clean:
-	rm -rf $(OUTDIR)
+	rm -f $(addprefix $(OUTDIR)/,$(targets))
 
-.PHONY: $(HTML) $(LATEX) $(PDF)
-$(HTML) $(LATEX) $(PDF): %: $(OUTDIR)/%
-
+ifneq ($(OUTDIR),.)
 $(OUTDIR):
 	mkdir -p $@
 
+# Specify the source to generated output mapping, e.g. `p2688r0.html` to `generated/p2688r0.html`
+.PHONY: $(targets)
+$(targets): %: $(OUTDIR)/%
+endif
+
+# Automatic rules to handle, e.g. `p2688r0.html : p2688r0.md`.
 $(OUTDIR)/%.html: %.md $(DEPS) | $(OUTDIR)
 	$(PANDOC)
 
